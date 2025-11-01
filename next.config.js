2e75d2a42ef97988441f2bc123f19f294e948ca3
KshaunishHarsha <notkshaun@gmail.com>
2025-11-01 12:40:15 +0530

    fix: configure webpack for pdf worker

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
       bodySizeLimit: "5mb",
     },
   },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.module.rules.push({
+      test: /pdf\.worker\.(min\.)?js/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/worker/[hash][ext][query]",
+      },
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
